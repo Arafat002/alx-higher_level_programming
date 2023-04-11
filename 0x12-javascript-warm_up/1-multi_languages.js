456f1ff33c67ec36af5a7ebfde27cdc3da5a979d
Arafat Motolani <mhyzdamilola@gmail.com>
2023-04-11 08:49:10 -0700

    fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-let c = 'C is fun';
-let python = 'Python is cool';
-let js = 'Javascript is amazing';
-console.log (c, python, js);
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'Javascript is amazing';
+console.log (c, + '\n' python, + '\n' , js);
